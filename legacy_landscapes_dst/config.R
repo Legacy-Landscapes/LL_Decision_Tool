84b2f89758098937fa451a122ac3c33f06851ee2
AlkeVoskamp <alke.voskamp@senckenberg.de>
2021-01-12 17:47:45 +0100

    More lint formatting. Deployment error replaced equal by arrow.

--- legacy_landscapes_dst/config.R
+++ legacy_landscapes_dst/config.R
@@ -8,9 +8,9 @@ background_sidepanel <-
   p(h5("Contact"),
   p(em("alke.voskamp@senckenberg.de")))
 
-ZGF_credits <- p("Frankfurt Zoological Society")
+zgf_credits <- p("Frankfurt Zoological Society")
 
-SGN_credits <- p(p("Senckenberg Biodiversity and Climate Research Centre"))
+sgn_credits <- p(p("Senckenberg Biodiversity and Climate Research Centre"))
 
 backround_mainpanel <-
   p(h4("Legacy Landscapes"),
@@ -20,11 +20,11 @@ backround_mainpanel <-
      Legacy Landscapes will significantly contribute to the ‘post-2020
      Biodiversity-Framework’ of the COP 15 at the CBD in 2020."),
   p(h5("The Legacy Landscapes concept is based on three pillars:")),
-  p(strong("1."),"Permanent, stable and performance-based funding ensured by
+  p(strong("1."), "Permanent, stable and performance-based funding ensured by
      a combination of private donors and public funds of about one
      million $ per site per year"),
-  p(strong("2."), "Effective and efficient management that will be caried out in
-     cooperation with national authorities and an NGO, with the annual
+  p(strong("2."), "Effective and efficient management that will be caried
+     out in cooperation with national authorities and an NGO, with the annual
      disbursement of funds being controlled by an independent platform,
      and based on the fulfillment of certain indicators, the ‘key
      performance indicators’."),
@@ -34,89 +34,103 @@ backround_mainpanel <-
      Research Centre to support the selection of suitable sites for the
      Legacy Landscapes fund. The app facilitates the ranking of sites based
      on their performance across six different conservation objectives:"),
-     strong("Biodiversity, Wilderness, Climatic stability, Land-use stability,
-     Climate protection and Size.")
+     strong("Biodiversity, Wilderness, Climatic stability, Land-use
+     stability, Climate protection and Size.")
   )
 
 background_figure3 <-
-  p(p(strong("Figure 1: The three cornerstones of the Legacy Landscapes concept"),
-      "permanent, stable and performance-based funding; effective and efficient
-       management and strategic site selection."))
+  p(p(strong("Figure 1: The three cornerstones of the Legacy
+             Landscapes concept"), "permanent, stable and performance-based
+             funding; effective and efficient management and strategic site
+             selection."))
 
 ## Conservation objectives panel
 objectives_weigting <-
   p("Use the sliders on the left to change the importance of the different
-     conservation objectives in the site ranking. The percentage weight allocated
-     to the different conservation objectives can be seen in the table on the right.",
-    p(em(strong("Note that combined allocated weights of the different conservation
-      objectives always sum up to 100%.")),style = "color:red"),style = "color:red")
+     conservation objectives in the site ranking. The percentage weight
+     allocated to the different conservation objectives can be seen in the
+     table on the right.",
+    p(em(strong("Note that combined allocated weights of the different
+     conservation objectives always sum up to 100%.")), style = "color:red"),
+    style = "color:red")
 
 objectives_strategy <-
   p(h4("The conservation objectives"),
-    p("Six conservation objectives were selected to enable the comparison between
-       eligible sites for the Legacy Landscapes fund based on macroecological data.
-       The objectives were chosen to allow a first assessment based on the size of the
-       site, the biodiversity it contains, its intactness and its potential for future
-       persistence. Each of the conservation objectives is measured based on one or more
-       macro-ecological variables as described below:"),
-    p(strong("Biodiversity:"),"includes the richness, endemism and diversity of species
-       comprising four different taxa (mammals, birds, reptiles and amphibians)"),
-    p(strong("Wilderness:"),"includes the Biodiversity Intactness Index (BII), the human
-       footprint and the observed change from biome to anthrome in the area over the past
-       20 years"),
-    p(strong("Climatic stability:"),"includes the projected stability of ecological
-       communities (mammals, birds, reptiles and amphibians) and the projected change
-       in tree cover by the mid of the century under a medium warming scenario"),
-    p(strong("Land-use stability:"),"includes the projected change in land-use in
-       the buffer zone around the site"),
-    p(strong("Climate protection:"),"includes the amount of baseline, vulnerable and
-       irreplaceable carbon storage within the site"),
-    p(strong("Size of the site:"),"is the extent of the site in km2"),
+    p("Six conservation objectives were selected to enable the comparison
+     between eligible sites for the Legacy Landscapes fund based on
+     macroecological data. The objectives were chosen to allow a first
+     assessment based on the size of the site, the biodiversity it contains,
+     its intactness and its potential for future persistence. Each of the
+     conservation objectives is measured based on one or more macro-ecological
+     variables as described below:"),
+    p(strong("Biodiversity:"), "includes the richness, endemism and diversity
+      of species comprising four different taxa (mammals, birds, reptiles and
+      amphibians)"),
+    p(strong("Wilderness:"), "includes the Biodiversity Intactness Index (BII),
+      the human footprint and the observed change from biome to anthrome
+      in the area over the past 20 years"),
+    p(strong("Climatic stability:"), "includes the projected stability of
+      ecological communities (mammals, birds, reptiles and amphibians) and
+      the projected change in tree cover by the mid of the century under a
+      medium warming scenario"),
+    p(strong("Land-use stability:"),"includes the projected change in land-use
+      in the buffer zone around the site"),
+    p(strong("Climate protection:"), "includes the amount of baseline,
+      vulnerable and irreplaceable carbon storage within the site"),
+    p(strong("Size of the site:"), "is the extent of the site in km2"),
     p("The six different conservation objectives can be combined into different
-       conservation goals as laid out in the figure below. Using the sliders on the
-       left the conservation objectives can be combined by allocating a weight to
-       each objective. Objectives allocated a weight of 0 are excluded from the weighting.
-       The resulting ranking based on the selected objectives and the allocated
-       importance (weight) can be seen in the", strong(em("Ranking Table")), "tab. The location
-       of the top scoring sites can be seen in the", strong(em("Ranking Map")), "tab."),
+       conservation goals as laid out in the figure below. Using the sliders on
+       the left the conservation objectives can be combined by allocating a
+       weight to each objective. Objectives allocated a weight of 0 are
+       excluded from the weighting. The resulting ranking based on the selected
+       objectives and the allocated importance (weight) can be seen in the",
+       strong(em("Ranking Table")), "tab. The location of the top scoring sites
+        can be seen in the", strong(em("Ranking Map")), "tab."),
     p(em("Details on the included variables, data sources and methods can be
           found on the accompanying webpage")))
 
 objectives_figure4 <-
   p(p(strong("Figure 2: Conservation objectives and strategies"),
-    "The six different conservation objectives Biodiversity, Wilderness, Climatic
-     stability, Land-use stability, Climate protection and Size can be combined into
-     different conservation goals. These conservation goals allow to weigh the different
-     conservation objectives against each other, to set priorities when selecting suitable
-     sites for the Legacy Landscapes fund."))
+    "The six different conservation objectives Biodiversity, Wilderness,
+    Climatic stability, Land-use stability, Climate protection and Size can
+    be combined into different conservation goals. These conservation goals
+    allow to weigh the different conservation objectives against each other,
+    to set priorities when selecting suitable sites for the Legacy Landscapes
+    fund."))
 
 ## Ranking table panel
 Rtable_text <-
-  p("The ranking table shows the overall ranking of the potential sites based on the applied weights.
-    The values for the different conservation objectives are scaled across all sites included
-    in the ranking. The values range from 0 to 1, with 0 being allocated to the site with the lowest score
-    and 1 being allocated to the site with the highest score for the conservation objective.",
-  p(em("The ranking table can be adjusted by using the sliders on the left hand side. Allocating
-  different weights to the individual objectives will change the ranking of the sites. Using the"),
-  em(strong("Select focal realm")), em("buttons above the table, the ranking table can be subset to show the resulting
-  ranking for the individual realms or across all sites globally.")))
+  p("The ranking table shows the overall ranking of the potential sites based
+    on the applied weights. The values for the different conservation
+    objectives are scaled across all sites included in the ranking. The values
+    range from 0 to 1, with 0 being allocated to the site with the lowest score
+    and 1 being allocated to the site with the highest score for the
+    conservation objective.",
+  p(em("The ranking table can be adjusted by using the sliders on the left
+    hand side. Allocating different weights to the individual objectives will
+    change the ranking of the sites. Using the"),
+  em(strong("Select focal realm")), em("buttons above the table, the ranking
+    table can be subset to show the resulting ranking for the individual
+    realms or across all sites globally.")))
 
 ## Ranking map panel
 Rmap_text <-
-  p("The map shows the location of the top sites ranked by their suitability based on the applied weights
-     across the six conservation objectives. Depending on the selection the map shows either the top 30
-     sites globally or the top 10 sites for a selected biogeographic realm.",
-  p("The white points show the locations for all sites included in the analysis. The red, orange and
-     yellow points show the location of the top sites with red indicating the sites of highest suitability.",
-  p(em("The choice of biogeographic realm can be changed by using the"), strong(em("Select focal realm")),
-   "button on the previous page."))
+  p("The map shows the location of the top sites ranked by their suitability
+    based on the applied weights across the six conservation objectives.
+    Depending on the selection the map shows either the top 30 sites globally
+    or the top 10 sites for a selected biogeographic realm.",
+  p("The white points show the locations for all sites included in the analysis.
+    The red, orange and yellow points show the location of the top sites with
+    red indicating the sites of highest suitability.",
+  p(em("The choice of biogeographic realm can be changed by using the"),
+    strong(em("Select focal realm")), "button on the previous page."))
   )
 
 Rmap_disclaimer <-
   p("*The country boarders displayed in this map are derived from Natural Earth
-    (version 4.1.0) and do not imply the expression of any opinion concerning the legal status of any
-    country, area or territory or of its authorities, or concerning the delamination of its boarders.",
-    style = "font-size:10px")
+    (version 4.1.0) and do not imply the expression of any opinion concerning
+    the legal status of any country, area or territory or of its authorities,
+    or concerning the delamination of its boarders.", style = "font-size:10px")
 
 
 #-#-# Set variable and subset names #-#-#
@@ -140,8 +154,13 @@ rownames_display <- list("biodiversity" = "Biodiversity",
                          "area" = "Size")
 
 ## subset names to display a selection of the data
-choices = list("Global" = "Global", "Afrotropic" = "Afrotropic", "Australasia" = "Australasia", "Indomalaya" = "Indomalaya",
-               "Nearctic" = "Nearctic", "Neotropic" = "Neotropic", "Palearctic" = "Palearctic")
+choices <- list("Global" = "Global",
+               "Afrotropic" = "Afrotropic",
+               "Australasia" = "Australasia",
+               "Indomalaya" = "Indomalaya",
+               "Nearctic" = "Nearctic",
+               "Neotropic" = "Neotropic",
+               "Palearctic" = "Palearctic")
 
 
 
